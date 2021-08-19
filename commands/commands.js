1471bd09d8c6b57c2ef9c452f6e408039b2e95ed
Windows - osu! - <72699183+windowsModern@users.noreply.github.com>
2021-08-19 19:31:08 +0000

    it's  not

--- commands/commands.js
+++ commands/commands.js
@@ -6,7 +6,7 @@ $description[Hi! I'm GenMusic, a free music bot with features like:
 > - Queue
 > - Loop
 > - Pause/Continue and such.
-I am currently in **$serversCount** servers and have **$allMembersCount** members.
+I am currently in **$serverCount** servers and have **$allMembersCount** members.
 *To get help about the commands please use \`$getVar[prefix]commands\`]
   
 `
